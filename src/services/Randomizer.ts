2aa44c916b4a819f6819b1ec4f8ad280ddc8548b
knowlesc <knowlesc8@gmail.com>
2019-11-24 14:11:52 -0800

    Improve randomization
    - Avoid duplicate categories in adjacent games
    - Don't use any letters twice until all have been seen

--- src/services/Randomizer.ts
+++ src/services/Randomizer.ts
@@ -2,21 +2,30 @@ import { Letters } from '../data/Letters';
 import { CategoryList } from '../data/CategoryList';
 
 export class Randomizer {
+  private static previousCategories: number[] = [];
+
   static getRandomCategories(count: number) {
-    return this.getRandomNumbers(count, CategoryList.length)
-      .map((index) => CategoryList[index]);
-  }
+    const categories: number[] = [];
 
-  static getRandomLetter() {
-    return Letters[this.getRandomNumbers(1, Letters.length)[0]];
-  }
-
-  private static getRandomNumbers(howMany: number, max: number) {
-    var numbers = [];
-    while(numbers.length < howMany) {
-        const next = Math.floor(Math.random() * max);
-        if(numbers.indexOf(next) === -1) numbers.push(next);
+    while (categories.length < count) {
+      const next = this.random(CategoryList.length);
+      if (this.previousCategories.indexOf(next) === -1
+        && categories.indexOf(next) === -1) {
+        categories.push(next);
+      }
     }
-    return numbers;
+
+    this.previousCategories = categories;
+    return categories.map((index) => CategoryList[index]);
+  }
+
+  private static unseenLetters = Letters;
+  static getRandomLetter() {
+    if (this.unseenLetters.length === 0) this.unseenLetters = Letters;
+    return this.unseenLetters[this.random(this.unseenLetters.length)];
+  }
+
+  private static random(max: number) {
+    return Math.floor(Math.random() * max);
   }
 }
