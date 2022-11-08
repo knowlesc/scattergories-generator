3c2463bb11d0acad7e447db788927ea41b0cf240
knowlesc <knowlesc8@gmail.com>
2022-11-08 14:21:01 -0800

    Drag this project into 2022

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,6 +1,6 @@
-import './Settings.scss';
-import * as React from 'react';
-import { RadioGroup } from '../RadioGroup/RadioGroup';
+import "./Settings.scss";
+import * as React from "react";
+import { RadioGroup } from "../RadioGroup/RadioGroup";
 
 export interface ISettingsProps {
   defaultMaxTime: number;
@@ -8,49 +8,57 @@ export interface ISettingsProps {
   onChange: (maxTime: number, categoryCount: number) => void;
 }
 
-export const Settings: React.FC<ISettingsProps> = ({ defaultMaxTime, defaultCategoryCount, onChange  }) => {
+export const Settings: React.FC<ISettingsProps> = ({
+  defaultMaxTime,
+  defaultCategoryCount,
+  onChange,
+}) => {
   const [visible, setVisible] = React.useState(false);
   const [maxTime, setMaxTime] = React.useState(defaultMaxTime);
-  const [categoryCount, setCategoryCount] = React.useState(defaultCategoryCount);
+  const [categoryCount, setCategoryCount] =
+    React.useState(defaultCategoryCount);
 
   const settingsClicked = () => {
     setVisible(!visible);
-  }
+  };
 
   const maxTimeUpdated = (value: number) => {
     setMaxTime(value);
     onChange(value, categoryCount);
-  }
+  };
 
   const categoryCountUpdated = (value: number) => {
     setCategoryCount(value);
     onChange(maxTime, value);
-  }
+  };
 
   return (
     <section className="settings-wrapper">
-      <div className="settings-button">
-        <i className="fas fa-cog"
-          onClick={ settingsClicked }></i>
+      <div className="settings-button" onClick={settingsClicked}>
+        <i className="fas fa-cog"></i>
       </div>
-      <div className={ `settings ${ visible ? 'visible' : '' }` }>
+      <div className={`settings ${visible ? "visible" : ""}`}>
         <div className="setting">
           <div className="setting-name">Timer</div>
           <div className="setting-value">
-            <RadioGroup values={ [90, 120, 150, 180] }
-              defaultValue={ defaultMaxTime }
-              onChange={ maxTimeUpdated } />
+            <RadioGroup
+              values={[90, 120, 150, 180]}
+              defaultValue={defaultMaxTime}
+              onChange={maxTimeUpdated}
+            />
           </div>
         </div>
         <div className="setting">
           <div className="setting-name">Categories</div>
           <div className="setting-value">
-            <RadioGroup values={ [10, 12, 15] }
-              defaultValue={ defaultCategoryCount }
-              onChange={ categoryCountUpdated } />
+            <RadioGroup
+              values={[10, 12, 15]}
+              defaultValue={defaultCategoryCount}
+              onChange={categoryCountUpdated}
+            />
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 };
